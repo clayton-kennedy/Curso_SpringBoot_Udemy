b9148d0a552066e22209f15f0858de210854b67b
Clayton Kennedy Passos dos Reis <Clayton.Reis@pg.df.gov.br>
2025-02-19 12:48:45 -0300

    Iniciando com sprign security

---
+++ src/main/java/com/cursospringboot/libraryapi/Config/SecurityConfiguration.java
@@ -0,0 +1,20 @@
+package com.cursospringboot.libraryapi.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf() //exige um token de validação em cada requisição POST, PUT, DELETE, etc.
+                .build();
+    }
+
+}
